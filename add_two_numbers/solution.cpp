876e197501e17b81fa5254cc4c6f79d527f5842b
DanMint <67702241+DanMint@users.noreply.github.com>
2020-07-01 19:32:40 +0300

    Create solution.cpp

---
+++ add_two_numbers/solution.cpp
@@ -0,0 +1,48 @@
+struct ListNode {
+    int val;
+    ListNode *next;
+    ListNode() : val(0), next(nullptr) {}
+    explicit ListNode(int x) : val(x), next(nullptr) {}
+};
+
+
+class Solution {
+public:
+    ListNode* addTwoNumbers (ListNode* l1, ListNode* l2) {
+        ListNode* l3 = nullptr;
+        ListNode* head = nullptr;
+        int carry = 0;
+
+        while (l1 != nullptr || l2 != nullptr) {
+            auto *newNode = new ListNode();
+
+            if (l3 == nullptr)
+                head = l3 = newNode;
+
+            else
+                l3 = l3->next = newNode;
+
+            const int num =
+                (l1 != nullptr ? l1->val : 0) +
+                (l2 != nullptr ? l2->val : 0) +
+                carry;
+
+            l3->val = num % 10;
+            carry = num / 10;
+
+            if (l1 != nullptr)
+                l1 = l1->next;
+
+            if (l2 != nullptr)
+                l2 = l2->next;
+        }
+
+        if (carry != 0) {
+            auto *lastNode = new ListNode();
+            l3 = l3->next = lastNode;
+            l3->val = carry;
+        }
+
+        return head;
+    }
+};
